046aab8a8289a2045bfa8c8d2e2c39bf2cea04e2
valyas01 <johana.ve96@gmail.com>
2015-10-03 22:40:48 -0600

    revisado

--- Prueba Junit/mainPruebas.java
+++ Prueba Junit/mainPruebas.java
@@ -1,3 +1,13 @@
+/*
+ 
+Universidad del valle de Guatemala
+Algoritmos y estructura de datos
+Hoja de trabajo # 8 
+Boris Cifuentes , 14150
+Yasmin Valdez, 14079
+
+*/
+
 // archivo main de las pruebas Junit
 //Aqui se prueba el VectorHeap
 public class mainPruebas {
