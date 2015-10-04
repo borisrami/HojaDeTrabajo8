10271e762fc980fa87f092247dd9e36551a7db83
valyas01 <johana.ve96@gmail.com>
2015-10-03 22:29:16 -0600

    Revisado

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,3 +1,13 @@
+/*
+ 
+Universidad del valle de Guatemala
+Algoritmos y estructura de datos
+Hoja de trabajo # 8 
+Boris Cifuentes , 14150
+Yasmin Valdez, 14079
+
+*/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,3 +1,13 @@
+/*
+ 
+Universidad del valle de Guatemala
+Algoritmos y estructura de datos
+Hoja de trabajo # 8 
+Boris Cifuentes , 14150
+Yasmin Valdez, 14079
+
+*/
+
 public interface PriorityQueue<E extends Comparable<E>> {
 	public E getFirst();
 	
