506e923d6caba51e1f0dd5d56c732a4849ebeee8
valyas01 <johana.ve96@gmail.com>
2015-10-03 22:40:25 -0600

    revisado

--- Prueba Junit/VectorHeap2.java
+++ Prueba Junit/VectorHeap2.java
@@ -1,3 +1,12 @@
+/*
+ 
+Universidad del valle de Guatemala
+Algoritmos y estructura de datos
+Hoja de trabajo # 8 
+Boris Cifuentes , 14150
+Yasmin Valdez, 14079
+
+*/
 import java.util.PriorityQueue;
 import java.util.Vector;
 //clase de VectoHeap para pruebaJunit
